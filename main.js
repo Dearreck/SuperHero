8b300b546b30a4c14c3b6f0fc756150fbaaedc36
Dearreck <dearreck.escoto@gmail.com>
2022-05-16 21:19:51 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,58 @@
+
+//localStorage.setItem("Datos", JSON.stringify(Heroes))
+//console.log(localStorage.getItem("Datos"))
+
+// Obtener el array del LocalStorage
+var Heroes = JSON.parse(localStorage.getItem("Datos"))
+
+function LoadHeroes() {
+
+    // Obtener el contenedor
+    let container = document.getElementById("container");
+
+    // Reset container
+    container.innerHTML = "";
+
+    // Cargar los datos
+    for (let index = 0; index < Heroes.length; index++) {
+        const element = `
+        <div id="div_heroe">
+                <img src="${Heroes[index].img}"><br>
+                <label id="nombre">${Heroes[index].nombre}</label><br>
+                <label id="bio">${Heroes[index].bio}</label><br>
+                <input type="button" id="btnDescription" value="Descripción" onclick="location.href='app.html?q=${Heroes[index].nombre}'">
+
+        </div>
+        `
+        //<input type="button" id="btnDescription" value="Descripción" onclick="alert('${Heroes[index].bio}')">
+        //
+
+        // Mostrar cada dato
+        container.innerHTML += element
+    }
+}
+
+function ShowMyHero(hero) {
+    // Cargar los datos
+    for (let index = 0; index < Heroes.length; index++) {
+
+        // Buscar mi heroe
+        if (Heroes[index].nombre == hero) {
+
+            const element = `
+                <div id="div_heroe">
+                        <img src="${Heroes[index].img}"><br>
+                        <label id="nombre">${Heroes[index].nombre}</label><br>
+                        <label id="fullBio">${Heroes[index].bio}</label><br>
+                        <input type="button" id="btnDescription" value="Regresar" onclick="location.href='.'">
+                </div>
+        `
+            // Mostrar cada dato
+            container.innerHTML += element
+
+            // Salir
+            return
+        }
+    }
+}
+
